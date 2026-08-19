8d7112a64cdc1bd020d1095646a30f8e6975de55
elkinjimenez7 <edanieljimenez@unicesar.edu.co>
2026-08-18 22:36:41 -0500

    feat: implementar READ de Cliente (listar y buscar)

--- src/main/java/com/mycompany/biblioteca/Customer.java
+++ src/main/java/com/mycompany/biblioteca/Customer.java
@@ -8,6 +8,31 @@ package com.mycompany.biblioteca;
  *
  * @author elkin jimenez
  */
-public class Customer {
+public class Customer extends Person {
+    
+    private String kindCustomer;
+    private int id;
+
+    public Customer(String kindCustomer, int id, String name, String cc, int age) {
+        super(name, cc, age);
+        this.kindCustomer = kindCustomer;
+        this.id = id;
+    }
+
+    public String getKindCustomer() {
+        return kindCustomer;
+    }
+
+    public void setKindCustomer(String kindCustomer) {
+        this.kindCustomer = kindCustomer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
     
 }
--- src/main/java/com/mycompany/biblioteca/Main.java
+++ src/main/java/com/mycompany/biblioteca/Main.java
@@ -4,11 +4,51 @@
 
 package com.mycompany.biblioteca;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author elkin jimenez
  */
 public class Main {
+    
+    static ArrayList<Customer> customer = new ArrayList<>();
+    static Scanner sc = new Scanner(System.in);
+    
+    public void createCustomer(String kindCustomer, int id, String name, String cc, int age){
+        
+        Customer newCustomer = new Customer(kindCustomer, id, name, cc, age);
+        
+        customer.add(newCustomer);
+    }
+    
+    public void listCustomers(JTable table){
+        DefaultTableModel model = new DefaultTableModel();
+        
+        model.addColumn("Tipo de Cliente");
+        model.addColumn("ID");
+        model.addColumn("Name");
+        model.addColumn("CC");
+        model.addColumn("Edad");
+        
+        for (Customer c : customer){
+            
+            Object[] row = new Object[5];
+            
+            row[0] = c.getKindCustomer();
+            row[1] = c.getId();
+            row[2] = c.getName();
+            row[3] = c.getCc();
+            row[4] = c.getAge();
+            
+            model.addRow(row);
+        }
+        table.setModel(model);
+       
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
