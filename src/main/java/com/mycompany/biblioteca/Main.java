d8c25a6d6b62d1a18b8704e2713f6f90ed650902
elkinjimenez7 <edanieljimenez@unicesar.edu.co>
2026-08-18 22:47:24 -0500

    feat: implementar READ de Cliente (buscar)

--- src/main/java/com/mycompany/biblioteca/Main.java
+++ src/main/java/com/mycompany/biblioteca/Main.java
@@ -49,6 +49,42 @@ public class Main {
         table.setModel(model);
        
     }
+    
+    public void searchCustomer(JTable table, String searchCc){
+        DefaultTableModel model = new DefaultTableModel();
+        
+        model.addColumn("Tipo de Cliente");
+        model.addColumn("ID");
+        model.addColumn("Name");
+        model.addColumn("CC");
+        model.addColumn("Edad");
+        
+        boolean found = false;
+        
+        for (Customer c : customer){
+            
+            if (c.getCc().equals(searchCc)){
+            
+            Object[] row = new Object[5];
+            
+            row[0] = c.getKindCustomer();
+            row[1] = c.getId();
+            row[2] = c.getName();
+            row[3] = c.getCc();
+            row[4] = c.getAge();
+            
+            model.addRow(row);
+            found = true;
+            break;
+           }
+        }
+        
+        if (!found){
+            javax.swing.JOptionPane.showMessageDialog(null, "El cliente con CC " + searchCc + " no esta registrado");
+        }
+        table.setModel(model);
+       
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
